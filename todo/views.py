83e9132a639ac75d9c3fc0cd0a205438ccd3274f
Vladimir Kornilov <vc-nte@mail.ru>
2022-02-10 17:26:24 +0300

    "End authentication"

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,7 @@
-from django.contrib.auth import login,logout
+from django.contrib.auth import login,logout,authenticate
 from django.db import IntegrityError
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
 # Create your views here.
@@ -31,3 +31,14 @@ def logoutuser(request):
     if request.method == 'POST':
         logout(request)
         return redirect('home')
+
+def loginuser(request):
+    if request.method == 'GET':
+        return render(request, "todo/loginuser.html", {'form': AuthenticationForm()})
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'],)
+        if user is None:
+            return render(request, "todo/loginuser.html", {'form': AuthenticationForm(), 'error': 'Username and password did not match'})
+        else:
+            login(request, user)
+            return redirect('currenttodos')
